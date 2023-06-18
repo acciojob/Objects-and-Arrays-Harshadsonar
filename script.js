18fec3c73e163f23c77b3df2c019bd9c3bc49429
Harshad Sonar <61082703+Harshadsonar@users.noreply.github.com>
2023-06-18 17:36:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-const players = ["John", "Bob", "Alice", "Poppy"];
+// players array
+var players = ["John", "Mike", "Sarah"];
 
-const person = {
-  name: "John Doe",
-  age: 80,
+// person object
+var person = {
+  name: "Alice",
+  age: 25,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+// Create a variable team that references players array
+var team = players;
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+// Create a variable team1 that is a copy of players array
+var team1 = players.slice();
+
+// Create a variable cap1 that is a copy of person object
+var cap1 = Object.assign({}, person);
+
+console.log(team);   // Output: ["John", "Mike", "Sarah"]
+console.log(team1);  // Output: ["John", "Mike", "Sarah"]
+console.log(cap1);   // Output: { name: "Alice", age: 25 }
